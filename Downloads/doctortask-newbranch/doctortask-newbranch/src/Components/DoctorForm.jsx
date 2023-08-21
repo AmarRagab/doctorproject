d71d9e906960938e2530341c4f490e153c52605c
Amar Zghier <202007617@bethlehem.edu>
2023-08-21 16:18:44 +0300

    final commit

---
+++ Downloads/doctortask-newbranch/doctortask-newbranch/src/Components/DoctorForm.jsx
@@ -0,0 +1,201 @@
+import React, { useState, useEffect } from "react";
+import Table from "./Table/Table";
+
+const DoctorForm = () => {
+  let thisregion = 'الجميع';
+  const [name, setName] = useState('');
+  const [specialization, setSpecialization] = useState('');
+  const [doctors, setDoctors] = useState([]);
+  const [filteredDoctors, setFilteredDoctors] = useState([]);
+  const [searchClicked, setSearchClicked] = useState(false);
+  const [majors, setMajors] = useState([]);
+  const [city, setCity] = useState([]);
+
+
+  useEffect(() => {
+    fetchData();
+
+
+  }, []);
+
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('https://www.mashreqins.com/ar/data/0/0/0?draw=1&columns%5B0%5D%5Bdata%5D=address&columns%5B0%5D%5Bname%5D=address&columns%5B0%5D%5Bsearchable%5D=true&columns%5B0%5D%5Borderable%5D=true&columns%5B0%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B0%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B1%5D%5Bdata%5D=major&columns%5B1%5D%5Bname%5D=major&columns%5B1%5D%5Bsearchable%5D=true&columns%5B1%5D%5Borderable%5D=true&columns%5B1%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B1%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B2%5D%5Bdata%5D=doctor&columns%5B2%5D%5Bname%5D=doctor&columns%5B2%5D%5Bsearchable%5D=true&columns%5B2%5D%5Borderable%5D=true&columns%5B2%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B2%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B3%5D%5Bdata%5D=mobile&columns%5B3%5D%5Bname%5D=mobile&columns%5B3%5D%5Bsearchable%5D=true&columns%5B3%5D%5Borderable%5D=true&columns%5B3%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B3%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B4%5D%5Bdata%5D=city&columns%5B4%5D%5Bname%5D=city&columns%5B4%5D%5Bsearchable%5D=true&columns%5B4%5D%5Borderable%5D=true&columns%5B4%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B4%5D%5Bsearch%5D%5Bregex%5D=false&order%5B0%5D%5Bcolumn%5D=0&order%5B0%5D%5Bdir%5D=asc&start=0&length=10&search%5Bvalue%5D=&search%5Bregex%5D=false&_=1691327610401');
+      const data = await response.json();
+      setDoctors([...data.data]);
+      setFilteredDoctors([...data.data]);
+      const forMajor = data.data.map((doctor) => doctor.major);
+      setMajors(['جميع التخصصات', ...forMajor]);
+      const forCity = data.data.map((doctor) => doctor.city);
+      const arrayforsort = ['الجميع', ...forCity.sort()];
+      for (let i = 0; i < arrayforsort.length; ++i) {
+        if (city.indexOf(arrayforsort[i]) < 0) {
+          city.push(arrayforsort[i])
+        }
+      }
+
+
+
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleSpecializationChange = (event) => {
+    setSpecialization(event.target.value);
+  };
+
+  const handleRegionClick = (region) => {
+    thisregion = region;
+    if (thisregion === 'الجميع') {
+      setFilteredDoctors(doctors);
+    }
+    applyFilters();
+
+
+  };
+
+  const applyFilters = () => {
+    if (thisregion === 'الجميع' && specialization.length === 0 && thisregion.length === 0) {
+      setFilteredDoctors(doctors);
+
+    }
+    else {
+      const filtered = doctors.filter((doctor) => {
+        const nameMatch = name.length === 0 || doctor.doctor.includes(name);
+        const specMatch = specialization.length === 0 || doctor.major === specialization || specialization === "جميع التخصصات";
+        const regionMatch = thisregion.length === 0 || doctor.city === thisregion || thisregion === "الجميع";
+        return nameMatch && specMatch && regionMatch;
+      });
+      setFilteredDoctors(filtered);
+    }
+
+  };
+
+
+
+
+  const searchDoctors = () => {
+    setSearchClicked(true);
+    applyFilters();
+
+
+  }
+
+
+
+
+
+
+
+
+  return (
+
+    <div className="main col-lg-12" dir="rtl">
+      <div className="bg-gray-800 text-white py-20">
+        <div className="container mx-auto text-center">
+          <h1 className="text-4xl font-bold mb-4">Find Your Perfect Doctor</h1>
+          <p className="text-lg mb-8">Search and connect with top-notch medical professionals.</p>
+          <a href="#search-form" className="inline-block">
+            <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-6 rounded-lg">
+              Search Doctors
+            </button>
+          </a>
+        </div>
+      </div>
+
+      <div
+        className="col-md-12 mt-4"
+        style={{ backgroundImage: 'url("https://www.mashreqins.com/assets/images/background-img-6.jpg")' }}
+      >
+
+
+        <h1 className="page-title text-2xl md:text-3xl text-center mt-4">الشبكة الطبية</h1>
+        <div id="search-form" className="col-md-12 mt-4">
+          <div className="flex justify-center items-center">
+            <div className="grid grid-cols-12 gap-4">
+              <div className="col-span-4">
+                <input
+                  className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+                  name="keyword"
+                  id="keyword"
+                  type="text"
+                  defaultValue=""
+                  placeholder="كلمة البحث"
+                  onChange={handleNameChange}
+                />
+              </div>
+              <div className="col-span-4">
+                <select
+                  className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+                  name="cat"
+                  id="cat"
+                  onChange={handleSpecializationChange}
+                >
+                  {majors.map((major, index) => {
+                    return <option key={major + index}>{major}</option>;
+                  })}
+                </select>
+              </div>
+              <div className="col-span-3">
+                <button
+                  type="button"
+                  name="medical_search"
+                  id="medical_search"
+                  className="w-full px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-200"
+                  onClick={searchDoctors}
+                >
+                  بحث
+                </button>
+              </div>
+            </div>
+          </div>
+
+
+
+        </div>
+
+        <div className="text-center">
+          <div className="flex justify-center overflow-x-scroll space-x-0 py-2">
+            {city.map((region, index) => (
+              <div className="group relative cursor-pointer flex-none" key={index}>
+                <button
+                  className={`block py-1 px-1.5 sm:px-4 text-gray-500 hover:text-blue-500 hover:bg-blue-50 transition duration-300 focus:outline-none rounded-lg ${thisregion === region ? "text-blue-500" : ""
+                    }`}
+                  id={"tab" + index}
+                  onClick={() => handleRegionClick(region)}
+
+                >
+                  {region}
+                </button>
+                {thisregion === region && (
+                  <span className="absolute inset-x-0 bottom-0 h-1 bg-blue-500 transform scale-x-0 transition-transform group-hover:scale-x-100 origin-center"></span>
+                )}
+              </div>
+            ))}
+          </div>
+        </div>
+
+
+      </div>
+
+
+      <div className="tab-content mt-4">
+        {filteredDoctors.length > 0 || searchClicked ? (
+          <Table data={[...filteredDoctors]} rowsPerPage={5}></Table>
+        ) : null}
+      </div>
+    </div>
+
+  );
+
+}
+
+export default DoctorForm;
---
+++ Downloads/doctortask-newbranch/doctortask-newbranch/src/Components/ReviewDialog.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import StarRating from './StarRating';
+import { useAuth } from './context/Auth/AuthContextProvider';
+
+const ReviewPopup = ({ onSave, onCancel, doctorId }) => {
+  const { user } = useAuth();
+  const [rating, setRating] = useState(0);
+  const [notes, setNotes] = useState('');
+
+
+
+  const handleSave = () => {
+    const reviewData = {
+      reviewer: user.username,
+      rating,
+      notes,
+      date: new Date().toISOString(),
+    };
+    onSave(reviewData);
+    setRating(0);
+    setNotes('');
+  };
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
+    <div className="bg-white p-6 rounded-lg shadow-md w-80">
+      <h2 className="text-2xl font-semibold mb-4">تقييم</h2>
+      <StarRating rating={rating} onRatingChange={setRating} />
+      <textarea
+        className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500 mt-4 resize-none"
+        rows="3"
+        value={notes}
+        onChange={(e) => setNotes(e.target.value)}
+        placeholder="أكتب ملاحظاتك..."
+      />
+      <div className="flex justify-center mt-6 space-x-4">
+        {rating > 0 && (
+          <button className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-200" onClick={onSave}>
+            حفظ
+          </button>
+        )}
+        <button className="bg-gray-500 text-white px-4 py-2 rounded-md hover:bg-gray-600 focus:outline-none focus:ring focus:ring-gray-200" onClick={onCancel}>
+          اغلاق
+        </button>
+      </div>
+    </div>
+  </div>
+
+
+  );
+};
+
+ReviewPopup.propTypes = {
+  onSave: PropTypes.func.isRequired,
+  onCancel: PropTypes.func.isRequired,
+  doctorId: PropTypes.number,
+};
+
+export default ReviewPopup;
